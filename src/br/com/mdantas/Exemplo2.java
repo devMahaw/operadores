4348c6237ff3956afd7db96e289a0dd00b7a1e76
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-01-28 14:40:53 -0300

    final project

---
+++ src/br/com/mdantas/Exemplo2.java
@@ -0,0 +1,13 @@
+package br.com.mdantas;
+
+/**
+ * @author marcelo.dantas
+ */
+public class Exemplo2 {
+
+    public static void main(String args[]) {
+        Exemplos exemplo = new Exemplos();
+
+
+    }
+}
